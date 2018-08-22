134e7d4f4cda4fe01b1986362462d2b87de0350b
Chrisz <1015973810@qq.com>
2018-08-22 11:05:56 +0800

    Update SingleFragmentActivity.java

--- criminalIntent/app/src/main/java/com/chrisz/android/criminalintent/SingleFragmentActivity.java
+++ criminalIntent/app/src/main/java/com/chrisz/android/criminalintent/SingleFragmentActivity.java
@@ -8,11 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
+    
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+//        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
